4a5770aa18f9b64f19a461c1d21662c1ac6f6879
SaLapus <onaera@bk.ru>
2025-04-10 15:35:10 +0300

    refactor: merging chapters and info stores

--- src/hooks/state/state.ts
+++ src/hooks/state/state.ts
@@ -8,135 +8,89 @@ import { fetchChaptersInfo, fetchTitleInfo } from "../../utils/api";
 import type { Data as ChapterInfo } from "../../types/api/ChaptersInfo";
 import type { TitleInfo } from "../../types/api/Title";
 
-interface Chapter extends ChapterInfo {
+export interface Chapter extends ChapterInfo {
   checked: boolean;
+
+  volumeID: number;
+  chapterFirstID: number;
+  chapterSecondID?: number;
 }
 interface InfoState {
   slug: string | undefined;
-  setSlug(slug: string): Promise<void>;
+  setSlug: (slug: string) => Promise<void>;
 
   titleInfo?: TitleInfo;
   chaptersInfo?: ChapterInfo[];
 
   chapters?: Record<number, Chapter>;
 
-  toggleChapter(chapterID: number): void;
-  allChapters(): void;
-  deleteChapters(): void;
+  toggleChapter: (chapterID: number) => void;
+  allChapters: () => void;
+  deleteChapters: () => void;
 }
 
 const useInfoStore = create<InfoState>()(
-  immer((set) => ({
-    slug: undefined,
-    setSlug: async (slug) => {
-      console.log("Setting slug");
+  devtools(
+    immer((set) => ({
+      slug: undefined,
+      setSlug: async (slug) => {
+        console.log("Setting slug");
 
-      try {
-        const [title, chapters] = await Promise.all([
-          fetchTitleInfo(slug),
-          fetchChaptersInfo(slug),
-        ]);
+        try {
+          const [title, chapters] = await Promise.all([
+            fetchTitleInfo(slug),
+            fetchChaptersInfo(slug),
+          ]);
 
-        console.log("Title fetched");
+          console.log("Title fetched");
 
-        set({ titleInfo: title, chaptersInfo: chapters, slug });
-
-        void Promise.resolve(() =>
           set({
+            titleInfo: title,
+            chaptersInfo: chapters,
+            slug,
             chapters: chapters.reduce((acc, cur) => {
-              acc[cur.id] = { ...cur, checked: false };
+              const [, first, second] = /(\d+)\.?(\d*)/.exec(cur.number)!;
+              const sec = parseInt(second, 10);
+              acc[cur.id] = {
+                ...cur,
+                volumeID: parseInt(cur.volume, 10),
+                chapterFirstID: parseInt(first, 10),
+                chapterSecondID: isNaN(sec) ? sec : undefined,
+                checked: false,
+              };
               return acc;
             }, {} as Record<number, Chapter>),
-          })
-        );
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    },
-
-    toggleChapter: (chapterID) =>
-      set((state: InfoState) => {
-        if (state.chapters)
-          state.chapters[chapterID].checked =
-            !state.chapters[chapterID].checked;
-      }),
-    allChapters() {
-      set((state: InfoState) => {
-        if (state.chapters) {
-          for (const key of Object.keys(state.chapters))
-            state.chapters[key as unknown as number].checked = true;
+          });
+        } catch (error) {
+          console.error("Error fetching data:", error);
         }
-      });
-    },
-    deleteChapters() {
-      set((state: InfoState) => {
-        if (state.chapters) {
-          for (const key of Object.keys(state.chapters))
-            state.chapters[key as unknown as number].checked = false;
-        }
-      });
-    },
-  }))
-);
+      },
 
-interface ChoosedChaptersState {
-  chapters: Chapter[];
-}
-
-const useChapterStore = create<ChoosedChaptersState>()(
-  devtools((set, get) => ({
-    chapters: [] as Chapter[],
-    ids: new Set(),
-    addChapters: (chapterID: number) => {
-      set({
-        chapters: new Map(chapters),
-        ids: new Set(
-          chapters.flatMap(([, val]) => val.map((chapter) => chapter.id))
-        ),
-      });
-    },
-    deleteChapters: (id) => {
-      const newState = new Map(get().chapters);
-
-      if (!id) set({ chapters: new Map(), ids: new Set() });
-      else {
-        newState.delete(id);
-
-        set({
-          chapters: newState,
-          ids: new Set(
-            [...newState.values()].flatMap((val) =>
-              val.map((chapter) => chapter.id)
-            )
-          ),
+      toggleChapter: (chapterID) =>
+        set((state: InfoState) => {
+          if (state.chapters)
+            state.chapters[chapterID].checked =
+              !state.chapters[chapterID].checked;
+        }),
+      allChapters: () => {
+        set((state: InfoState) => {
+          if (state.chapters) {
+            for (const key of Object.keys(state.chapters))
+              state.chapters[key as unknown as number].checked = true;
+          }
         });
-      }
-    },
-    toggleChapter: (vol, chapter) => {
-      const newState = new Map(get().chapters);
-
-      if (
-        [...newState.values()]
-          .flatMap((val) => val.map((chapter) => chapter.id))
-          .find((id) => id === chapter.id)
-      )
-        newState.delete(vol);
-      else if (newState.has(vol)) newState.get(vol)?.push(chapter);
-      else newState.set(vol, [chapter]);
-
-      set({
-        chapters: newState,
-        ids: new Set(
-          [...newState.values()].flatMap((val) =>
-            val.map((chapter) => chapter.id)
-          )
-        ),
-      });
-    },
-
-    hasChapter: (id) => get().ids.has(id),
-  }))
+      },
+      deleteChapters: () => {
+        set((state: InfoState) => {
+          if (state.chapters) {
+            for (const key of Object.keys(state.chapters))
+              state.chapters[key as unknown as number].checked = false;
+          }
+        });
+      },
+    }))
+  )
 );
 
-export { useChapterStore, useInfoStore };
+export { useInfoStore };
 
