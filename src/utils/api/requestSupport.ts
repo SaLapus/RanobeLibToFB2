53edb56f89d081eee4c0aa539a36e78fb47ccbed
SaLapus <onaera@bk.ru>
2025-04-10 15:37:03 +0300

    refactor: better typisation for support function

--- src/utils/api/requestSupport.ts
+++ src/utils/api/requestSupport.ts
@@ -2,25 +2,26 @@ import Awaiter from "./awaiter";
 
 const awaiter = new Awaiter();
 
-export default function requestSupport<Input, Output>(
-  reqFunc: (...value: Input[]) => Promise<Output>
-): (...value: Input[]) => Promise<Output> {
-  return async (...value: Input[]) => {
-    let returnValue: Output;
-    let attempts = 0;
-
+export default function requestSupport<
+  Args extends unknown[],
+  Return extends Promise<unknown>
+>(reqFunc: (...args: Args) => Return): (...args: Args) => Promise<Awaited<Return>> {
+  return async (
+    ...value: Args
+  ): Promise<Awaited<Return>> => {
     do {
+      let attempts = 0;
       try {
         await awaiter.next();
 
-        returnValue = await reqFunc(...value);
-
-        return returnValue;
+        return await reqFunc(...value);
       } catch (e) {
         attempts++;
         console.error(e);
         if (attempts === 10)
-          throw `Too many (10) attempts at ${reqFunc.name}\nArgs: ${value}`;
+          console.error(
+            `Too many (10) attempts at ${reqFunc.name}\nArgs: ${value.join()}`
+          );
       }
       // eslint-disable-next-line no-constant-condition
     } while (true);
