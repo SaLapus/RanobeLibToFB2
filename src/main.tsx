c7af88725e939d2e206ad017b6011910948e21df
SaLapus <onaera@bk.ru>
2025-04-16 18:04:38 +0300

    feat: start of firebase setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+declare global {
+  const __TARGET__: string;
+}
+if (__TARGET__ === "web") {
+  void import("./firebase.ts");
+}
+
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -5,33 +5,51 @@ import { defineConfig } from "vite";
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
-export default defineConfig({
-  // prevent vite from obscuring rust errors
-  clearScreen: false,
-  server: {
-    // Tauri expects a fixed port, fail if that port is not available
-    strictPort: true,
-    // if the host Tauri is expecting is set, use it
-    host: host ?? false,
-    port: 5173,
-  },
-  // Env variables starting with the item of `envPrefix` will be exposed in tauri's source code through `import.meta.env`.
-  envPrefix: ["VITE_", "TAURI_ENV_*"],
-  build: {
-    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
-    target:
-      process.env.TAURI_ENV_PLATFORM == "windows" ? "chrome105" : "safari13",
-    // don't minify for debug builds
-    minify: !process.env.TAURI_ENV_DEBUG ? "esbuild" : false,
-    // produce sourcemaps for debug builds
-    // sourcemap: !!process.env.TAURI_ENV_DEBUG,
-    sourcemap: true,
-  },
+export default defineConfig((config) => {
+  console.log(config);
+  
+  return {
+    // prevent vite from obscuring rust errors
+    define: {
+      __TARGET__: config.mode,
+    },
+    clearScreen: false,
+    server: {
+      // Tauri expects a fixed port, fail if that port is not available
+      strictPort: true,
+      // if the host Tauri is expecting is set, use it
+      host: host ?? false,
+      port: 5173,
+    },
+    // Env variables starting with the item of `envPrefix` will be exposed in tauri's source code through `import.meta.env`.
+    envPrefix: ["FIREBASE_", "VITE_", "TAURI_ENV_*"],
+    build: {
+      outDir: config.mode === "tauri" ? "dist/tauri" : "dist/web",
+      // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+      target:
+        process.env.TAURI_ENV_PLATFORM == "windows" ? "chrome105" : "safari13",
+      // don't minify for debug builds
+      minify: !process.env.TAURI_ENV_DEBUG ? "esbuild" : false,
+      // produce sourcemaps for debug builds
+      // sourcemap: !!process.env.TAURI_ENV_DEBUG,
+      sourcemap: process.env.NODE_ENV !== "production",
 
-  plugins: [
-    react(),
-    wyw({
-      displayName: process.env.NODE_ENV !== "production",
-    }),
-  ],
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            react: ["react", "react-dom"],
+            firebase: ["firebase/app", "firebase/analytics"],
+            xml: ["xmlbuilder2"],
+          },
+        },
+      },
+    },
+
+    plugins: [
+      react(),
+      wyw({
+        displayName: process.env.NODE_ENV !== "production",
+      }),
+    ],
+  };
 });
