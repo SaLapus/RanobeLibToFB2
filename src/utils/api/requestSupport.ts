2415d44b8917e03b535f342eca0af6e4de9243a2
SaLapus <onaera@bk.ru>
2025-04-22 17:57:23 +0300

    fix: streamline requestSupport function by improving error handling and removing infinity loop

--- src/utils/api/requestSupport.ts
+++ src/utils/api/requestSupport.ts
@@ -5,25 +5,31 @@ const awaiter = new Awaiter();
 export default function requestSupport<
   Args extends unknown[],
   Return extends Promise<unknown>
->(reqFunc: (...args: Args) => Return): (...args: Args) => Promise<Awaited<Return>> {
-  return async (
-    ...value: Args
-  ): Promise<Awaited<Return>> => {
-    do {
-      let attempts = 0;
+>(
+  reqFunc: (...args: Args) => Return
+): (...args: Args) => Promise<Awaited<Return>> {
+  return async (...value: Args): Promise<Awaited<Return>> => {
+    let attempts = 0;
+
+    while (attempts < 10) {
       try {
         await awaiter.next();
 
+        // Fix to remove this await
+        // Or try understand why it needs from logic point
         return await reqFunc(...value);
       } catch (e) {
         attempts++;
         console.error(e);
-        if (attempts === 10)
-          console.error(
-            `Too many (10) attempts at ${reqFunc.name}\nArgs: ${value.join()}`
-          );
+
+        await new Promise((resolve) =>
+          setTimeout(resolve, Math.min(1000 * attempts, 5000))
+        );
       }
-      // eslint-disable-next-line no-constant-condition
-    } while (true);
+    }
+
+    throw new Error(
+      `Too many (10) attempts at ${reqFunc.name}\nArgs: ${value.join()}`
+    );
   };
 }
